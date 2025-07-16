e08e556826ceffad2ebaf86d582e24dfbc7e3eda
livvvvvvvie <izzymoonuhxd@gmail.com>
2025-07-16 14:58:40 +0900

    작업 중인 변경사항 임시 커밋

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/movie_app_2025/',
   plugins: [
     react({
       babel: {
         plugins: [
           [
             'babel-plugin-transform-react-remove-prop-types',
-            { removeImport: false } // ✅ 이 안쪽에 옵션 넣어야 함
+            { removeImport: false } 
           ]
         ]
       }
     })
   ]
-});
+}));
